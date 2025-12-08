9124e35d94d23bc2cd3c81ad87221d369ef03fdc
Maksym Halushko <gallikillag@gmail.com>
2025-12-08 20:59:35 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const headers = tree.querySelectorAll('li');
+
+headers.forEach((header) => {
+  const span = document.createElement('span');
+
+  if (header.firstElementChild !== null) {
+    span.textContent = header.firstChild.textContent;
+    header.firstChild.replaceWith(span);
+  }
+});
+
+const newSpan = document.querySelectorAll('span');
+
+newSpan.forEach((span) => {
+  if (!span) {
+    return;
+  }
+
+  span.addEventListener('click', () => {
+    if (span.nextElementSibling.hidden === true) {
+      span.nextElementSibling.hidden = false;
+    } else {
+      span.nextElementSibling.hidden = true;
+    }
+  });
+});
