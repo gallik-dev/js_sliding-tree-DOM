f02c73a33d8fcfc14d82fde783624893f1775ddc
Maksym Halushko <gallikillag@gmail.com>
2025-12-09 14:39:29 +0200

    add new solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,22 @@ const tree = document.querySelector('.tree');
 const headers = tree.querySelectorAll('li');
 
 headers.forEach((header) => {
+  let textNode = null;
+
+  for (const node of header.childNodes) {
+    if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '') {
+      textNode = node;
+      break;
+    }
+  }
+
+  if (!textNode) {
+    return;
+  }
+
   const span = document.createElement('span');
 
-  span.textContent = header.firstChild.textContent;
+  span.textContent = textNode.textContent.trim();
   header.firstChild.replaceWith(span);
 });
 
